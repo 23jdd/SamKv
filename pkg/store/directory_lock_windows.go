7c602424a6bc379b8f1d7e6e1bac90a49e6e5901
long <ab1234_1123@qq.com>
2026-07-24 14:59:19 +0800

    store: keep Windows lock owner metadata readable

--- pkg/store/directory_lock_windows.go
+++ pkg/store/directory_lock_windows.go
@@ -9,8 +9,10 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+const lockRegionOffset = 1 << 30
+
 func tryLockFile(file *os.File) error {
-	var overlapped windows.Overlapped
+	overlapped := windows.Overlapped{Offset: lockRegionOffset}
 	err := windows.LockFileEx(
 		windows.Handle(file.Fd()),
 		windows.LOCKFILE_EXCLUSIVE_LOCK|windows.LOCKFILE_FAIL_IMMEDIATELY,
@@ -26,6 +28,6 @@ func tryLockFile(file *os.File) error {
 }
 
 func unlockFile(file *os.File) error {
-	var overlapped windows.Overlapped
+	overlapped := windows.Overlapped{Offset: lockRegionOffset}
 	return windows.UnlockFileEx(windows.Handle(file.Fd()), 0, 1, 0, &overlapped)
 }
