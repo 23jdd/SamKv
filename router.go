c0ad058f1e22cad8d23e7162868fb30e7273a525
long <ab1234_1123@qq.com>
2026-08-07 10:36:58 +0800

    Rename .env to .env.example

--- router.go
+++ router.go
@@ -8,7 +8,6 @@ import (
 	"errors"
 	"io"
 	"net/http"
-	"os"
 	"strings"
 
 	"github.com/23jdd/SamKv/pkg/store"
@@ -57,8 +56,6 @@ func NewRouter(database KVStore) *gin.Engine {
 	if database == nil {
 		panic("router: nil store")
 	}
-	os.Rename()
-    
 	router := gin.New()
 	router.Use(gin.Logger(), gin.Recovery())
 	router.HandleMethodNotAllowed = true
